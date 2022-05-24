b08977f0c7ec9b3a1fd42736d9db7ff01d631ef2
Adrenix <15348635+Adrenix@users.noreply.github.com>
2022-05-24 00:55:42 -0400

    Fix injection failure due to generic parameter

--- common/src/main/java/mod/adrenix/nostalgic/mixin/client/ThrownItemRendererMixin.java
+++ common/src/main/java/mod/adrenix/nostalgic/mixin/client/ThrownItemRendererMixin.java
@@ -12,14 +12,14 @@ import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(ThrownItemRenderer.class)
-public abstract class ThrownItemRendererMixin
+public abstract class ThrownItemRendererMixin <T extends Entity>
 {
     /**
      * Makes thrown items, like snowballs, render in 2D.
      * Controlled by the old 2D throwing toggle.
      */
-    @Inject(method = "render", at = @At(shift = At.Shift.AFTER, ordinal = 1, value = "INVOKE", target = "Lcom/mojang/blaze3d/vertex/PoseStack;mulPose(Lcom/mojang/math/Quaternion;)V"))
-    protected <T extends Entity> void onRenderScale(T entity, float entityYaw, float partialTicks, PoseStack poseStack, MultiBufferSource buffer, int packedLight, CallbackInfo callback)
+    @Inject(method = "render(Lnet/minecraft/world/entity/Entity;FFLcom/mojang/blaze3d/vertex/PoseStack;Lnet/minecraft/client/renderer/MultiBufferSource;I)V", at = @At(shift = At.Shift.BEFORE, value = "INVOKE", target = "Lnet/minecraft/client/renderer/entity/ItemRenderer;renderStatic(Lnet/minecraft/world/item/ItemStack;Lnet/minecraft/client/renderer/block/model/ItemTransforms$TransformType;IILcom/mojang/blaze3d/vertex/PoseStack;Lnet/minecraft/client/renderer/MultiBufferSource;I)V"))
+    protected void onRenderScale(T entity, float entityYaw, float partialTicks, PoseStack poseStack, MultiBufferSource buffer, int packedLight, CallbackInfo callback)
     {
         if (MixinConfig.Candy.oldFlatThrowing())
         {
@@ -32,8 +32,8 @@ public abstract class ThrownItemRendererMixin
      * Disables the diffused lighting before item rendering and re-enables after the rendering process is complete.
      * Controlled by the old 2D throwing toggle.
      */
-    @Inject(method = "render", at = @At(shift = At.Shift.AFTER, value = "INVOKE", target = "Lnet/minecraft/client/renderer/entity/ItemRenderer;renderStatic(Lnet/minecraft/world/item/ItemStack;Lnet/minecraft/client/renderer/block/model/ItemTransforms$TransformType;IILcom/mojang/blaze3d/vertex/PoseStack;Lnet/minecraft/client/renderer/MultiBufferSource;I)V"))
-    protected <T extends Entity> void onRenderFinish(T entity, float entityYaw, float partialTicks, PoseStack poseStack, MultiBufferSource buffer, int packedLight, CallbackInfo callback)
+    @Inject(method = "render(Lnet/minecraft/world/entity/Entity;FFLcom/mojang/blaze3d/vertex/PoseStack;Lnet/minecraft/client/renderer/MultiBufferSource;I)V", at = @At(shift = At.Shift.AFTER, value = "INVOKE", target = "Lnet/minecraft/client/renderer/entity/ItemRenderer;renderStatic(Lnet/minecraft/world/item/ItemStack;Lnet/minecraft/client/renderer/block/model/ItemTransforms$TransformType;IILcom/mojang/blaze3d/vertex/PoseStack;Lnet/minecraft/client/renderer/MultiBufferSource;I)V"))
+    protected void onRenderFinish(T entity, float entityYaw, float partialTicks, PoseStack poseStack, MultiBufferSource buffer, int packedLight, CallbackInfo callback)
     {
         MixinUtil.Item.enableDiffusedLighting();
     }
