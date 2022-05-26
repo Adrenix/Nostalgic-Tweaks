f7b29f2293ab290a8f8ef8a4c84f449155f4025d
Adrenix <15348635+Adrenix@users.noreply.github.com>
2022-05-26 12:32:59 -0400

    Fix leaves block AO injection failure

--- common/src/main/java/mod/adrenix/nostalgic/mixin/common/LeavesBlockMixin.java
+++ common/src/main/java/mod/adrenix/nostalgic/mixin/common/LeavesBlockMixin.java
@@ -3,37 +3,28 @@ package mod.adrenix.nostalgic.mixin.common;
 import mod.adrenix.nostalgic.client.config.MixinConfig;
 import net.minecraft.core.BlockPos;
 import net.minecraft.world.level.BlockGetter;
+import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.LeavesBlock;
 import net.minecraft.world.level.block.state.BlockState;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Unique;
-import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(LeavesBlock.class)
-public abstract class LeavesBlockMixin
+public abstract class LeavesBlockMixin extends Block
 {
-    /**
-     * Overrides the light blocking to simulate old leaves from alpha/beta.
-     * Controlled by the old lighting toggle.
-     */
-
-    @Inject(method = "getLightBlock", at = @At(value = "HEAD"), cancellable = true)
-    protected void onGetLightBlock(BlockState state, BlockGetter level, BlockPos pos, CallbackInfoReturnable<Integer> callback)
+    private LeavesBlockMixin(Properties ignored)
     {
-        callback.setReturnValue(MixinConfig.Candy.oldLighting() ? 0 : 1);
+        super(ignored);
     }
 
     /**
-     * Overrides shade brightness to simulate old leaves shading from alpha/beta.
-     * Controlled by the old lighting toggle.
+     * Overrides shade brightness so AO can be removed from touching full blocks.
+     * Controlled by the old leaves lighting toggle.
      */
 
-    @Unique
-    @SuppressWarnings("unused")
+    @Override
+    @SuppressWarnings("deprecation")
     public float getShadeBrightness(BlockState state, BlockGetter level, BlockPos pos)
     {
-        return MixinConfig.Candy.oldLighting() ? 1.0F : state.isCollisionShapeFullBlock(level, pos) ? 0.2F : 1.0F;
+        return MixinConfig.Candy.oldLeavesLighting() ? 1.0F : super.getShadeBrightness(state, level, pos);
     }
 }
