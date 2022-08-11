ef4476f201d5d156ca291ea52fe4b3fe0ff27e46
ruvaldak <tyler.bonnette98@gmail.com>
2022-08-10 23:59:54 -0400

    lets try that again

--- common/remappedSrc/mod/adrenix/nostalgic/client/config/gui/widget/IPermissionWidget.java
+++
@@ -1,7 +0,0 @@
-package mod.adrenix.nostalgic.client.config.gui.widget;
-
-/**
- * Used to determine if a widget can be disabled depending on if the player is a server operator.
- */
-
-public interface IPermissionWidget {}
--- common/remappedSrc/mod/adrenix/nostalgic/common/event/CommonEventHelper.java
+++
@@ -1,9 +0,0 @@
-package mod.adrenix.nostalgic.common.event;
-
-/**
- * This class will be class loaded by both the client and server.
- * Keep vanilla client-only code out of this class.
- */
-
-@SuppressWarnings("unused") // Place holder
-public abstract class CommonEventHelper { }
