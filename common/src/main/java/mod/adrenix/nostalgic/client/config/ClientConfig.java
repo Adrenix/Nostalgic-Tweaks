cc42064305a7903feaa9dd44184608ccfe9be749
Adrenix <15348635+Adrenix@users.noreply.github.com>
2022-09-11 15:40:18 -0400

    Disabled square chunk border tweak

--- common/src/main/java/mod/adrenix/nostalgic/client/config/ClientConfig.java
+++ common/src/main/java/mod/adrenix/nostalgic/client/config/ClientConfig.java
@@ -842,8 +842,9 @@ public class ClientConfig implements ConfigData
          */
 
         @TweakSide.Server
-        @TweakClient.Run.ReloadChunks
         @TweakSide.EntryStatus
+        @TweakClient.Run.ReloadChunks
+        @TweakClient.Gui.Warning
         @TweakClient.Gui.Cat(group = TweakClient.Category.WORLD_CANDY)
         public boolean oldSquareBorder = DefaultConfig.Candy.OLD_SQUARE_BORDER;
         static { CandyTweak.SQUARE_BORDER.setKey("oldSquareBorder"); }
--- common/src/main/java/mod/adrenix/nostalgic/mixin/common/server/ChunkMapMixin.java
+++ common/src/main/java/mod/adrenix/nostalgic/mixin/common/server/ChunkMapMixin.java
@@ -4,8 +4,7 @@ import mod.adrenix.nostalgic.common.config.ModConfig;
 import mod.adrenix.nostalgic.util.server.WorldServerUtil;
 import net.minecraft.server.level.ChunkMap;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.*;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(ChunkMap.class)
@@ -15,10 +14,10 @@ public abstract class ChunkMapMixin
      * Forces the chunk border to render as a square instead of a disc.
      * Controlled by the old square chunk border tweak.
      */
-    @Inject(method = "isChunkInRange", at = @At(value = "HEAD"), cancellable = true)
-    private static void NT$onIsChunkRangeBorder(int chunkX, int chunkZ, int secX, int secZ, int viewDistance, CallbackInfoReturnable<Boolean> callback)
-    {
-        if (ModConfig.Candy.oldSquareBorder())
-            callback.setReturnValue(WorldServerUtil.isChunkInRange(chunkX, chunkZ, secX, secZ, viewDistance));
-    }
+//    @Inject(method = "isChunkInRange", at = @At(value = "HEAD"), cancellable = true)
+//    private static void NT$onIsChunkRange(int chunkX, int chunkZ, int secX, int secZ, int viewDistance, CallbackInfoReturnable<Boolean> callback)
+//    {
+//        if (ModConfig.Candy.oldSquareBorder())
+//            callback.setReturnValue(WorldServerUtil.isChunkInRange(chunkX, chunkZ, secX, secZ, viewDistance));
+//    }
 }
