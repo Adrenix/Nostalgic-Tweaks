fb8c36b31b4a9241946a6e5ea885ca65ada6e125
Adrenix <15348635+Adrenix@users.noreply.github.com>
2022-09-11 16:06:12 -0400

    Fixed colormatic override of lever particles tweak

--- common/src/main/java/mod/adrenix/nostalgic/mixin/common/world/level/block/LeverBlockMixin.java
+++ common/src/main/java/mod/adrenix/nostalgic/mixin/common/world/level/block/LeverBlockMixin.java
@@ -1,31 +1,30 @@
 package mod.adrenix.nostalgic.mixin.common.world.level.block;
 
 import mod.adrenix.nostalgic.common.config.ModConfig;
+import net.minecraft.core.BlockPos;
+import net.minecraft.world.level.LevelAccessor;
 import net.minecraft.world.level.block.LeverBlock;
+import net.minecraft.world.level.block.state.BlockState;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.ModifyArg;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(LeverBlock.class)
 public abstract class LeverBlockMixin
 {
     /**
-     * Disables the redstone particles emitted by levers when activated. This is done by modifying the alpha value to be
-     * fully transparent.
+     * Disables the redstone particles emitted by levers when activated.
+     *
+     * Since many users of N.T. will be using the colormatic mod, this injection will cancel at the HEAD position.
+     * Modifying the alpha argument in the particle options won't work since colormatic redirects this method.
      *
      * Controlled by the disable lever particles tweak.
      */
-    @ModifyArg
-    (
-        method = "makeParticle",
-        at = @At
-        (
-            value = "INVOKE",
-            target = "Lnet/minecraft/core/particles/DustParticleOptions;<init>(Lcom/mojang/math/Vector3f;F)V"
-        )
-    )
-    private static float NT$onMakeParticle(float alpha)
+    @Inject(method = "makeParticle", at = @At("HEAD"), cancellable = true)
+    private static void NT$onMakeParticle(BlockState state, LevelAccessor level, BlockPos pos, float alpha, CallbackInfo callback)
     {
-        return ModConfig.Candy.disableLeverParticles() ? 0.0F : alpha;
+        if (ModConfig.Candy.disableLeverParticles())
+            callback.cancel();
     }
 }
