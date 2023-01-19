39e2d89a8330567c6340ba09ccf51e3a80bf2073
Adrenix <15348635+Adrenix@users.noreply.github.com>
2023-01-19 16:44:20 -0500

    Redid old overlay rendering and changed toast ticking

--- fabric/src/main/java/mod/adrenix/nostalgic/fabric/event/ClientEvents.java
+++ fabric/src/main/java/mod/adrenix/nostalgic/fabric/event/ClientEvents.java
@@ -1,10 +1,8 @@
 package mod.adrenix.nostalgic.fabric.event;
 
-import mod.adrenix.nostalgic.client.config.gui.toast.NostalgicToast;
 import mod.adrenix.nostalgic.fabric.api.event.NostalgicHudEvent;
 import mod.adrenix.nostalgic.fabric.event.client.CandyEvents;
 import mod.adrenix.nostalgic.fabric.event.client.ClientNetworkEvents;
-import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
 
 /**
  * Registers client-side Fabric events.
@@ -21,6 +19,5 @@ public abstract class ClientEvents
         CandyEvents.register();
         ClientNetworkEvents.register();
         NostalgicHudEvent.register();
-        ClientTickEvents.END_CLIENT_TICK.register(client -> NostalgicToast.tick());
     }
 }
