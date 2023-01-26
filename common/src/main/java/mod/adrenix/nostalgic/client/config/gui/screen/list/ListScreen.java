d5bcbcf8dfd9607c8454199a108e746600e7c92d
Adrenix <15348635+Adrenix@users.noreply.github.com>
2023-01-26 17:49:13 -0500

    Fixed overlap buttons not displaying correctly with resource packs that have button border highlights

--- common/src/main/java/mod/adrenix/nostalgic/client/config/gui/screen/list/ListScreen.java
+++ common/src/main/java/mod/adrenix/nostalgic/client/config/gui/screen/list/ListScreen.java
@@ -975,6 +975,13 @@ public abstract class ListScreen extends ConfigScreen
         for (Renderable widget : this.listWidgets)
             this.renderWidget(widget, poseStack, mouseX, mouseY, partialTick);
 
+        // Mouse overlap highlight
+        for (Renderable widget : this.listWidgets)
+        {
+            if (widget instanceof OverlapButton button && button.isMouseOver(mouseX, mouseY))
+                widget.render(poseStack, mouseX, mouseY, partialTick);
+        }
+
         // Screen title rendering
         drawCenteredString(poseStack, this.font, this.title.getString(), this.width / 2, 8, 0xFFFFFF);
 
