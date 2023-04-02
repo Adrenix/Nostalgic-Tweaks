416485996da865e3e2fc130201923cfb05f9df96
Adrenix <15348635+Adrenix@users.noreply.github.com>
2023-04-02 17:10:04 -0400

    Fixed improper widget focusing in config menu lists

--- common/src/main/java/mod/adrenix/nostalgic/client/config/gui/widget/list/AbstractEntry.java
+++ common/src/main/java/mod/adrenix/nostalgic/client/config/gui/widget/list/AbstractEntry.java
@@ -1,17 +1,35 @@
 package mod.adrenix.nostalgic.client.config.gui.widget.list;
 
 import net.minecraft.client.gui.components.ContainerObjectSelectionList;
+import net.minecraft.client.gui.components.events.GuiEventListener;
+
+import java.util.Optional;
+
+/**
+ * The mod's abstract row list implements its own method of focusing widgets. These overrides prevent the game from
+ * disrupting that implementation.
+ */
 
 public abstract class AbstractEntry<E extends ContainerObjectSelectionList.Entry<E>> extends ContainerObjectSelectionList.Entry<E>
 {
-    /**
-     * The mod's abstract row list implements its own method of focusing widgets. This override prevents the game from
-     * disrupting that implementation.
-     */
     @Override
-    public boolean mouseReleased(double mouseX, double mouseY, int button)
+    public boolean mouseDragged(double mouseX, double mouseY, int button, double dragX, double dragY)
     {
+        Optional<GuiEventListener> child = this.getChildAt(mouseX, mouseY);
+
+        if (child.isPresent() && this.isDragging() && button == 0)
+            return child.get().mouseDragged(mouseX, mouseY, button, dragX, dragY);
+
+        return false;
+    }
+
+    @Override
+    public boolean mouseClicked(double mouseX, double mouseY, int button)
+    {
+        boolean clicked = super.mouseClicked(mouseX, mouseY, button);
+
         this.setFocused(null);
-        return super.mouseReleased(mouseX, mouseY, button);
+
+        return clicked;
     }
 }
