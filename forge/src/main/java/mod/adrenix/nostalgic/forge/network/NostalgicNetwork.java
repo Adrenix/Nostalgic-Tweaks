f7fb811823d709f303ecd4180ba1501437a40a16
Adrenix <15348635+Adrenix@users.noreply.github.com>
2023-04-08 21:20:12 -0400

    (Cherry pick) Added experimental SSO mode for servers

--- forge/src/main/java/mod/adrenix/nostalgic/forge/network/NostalgicNetwork.java
+++ forge/src/main/java/mod/adrenix/nostalgic/forge/network/NostalgicNetwork.java
@@ -1,6 +1,7 @@
 package mod.adrenix.nostalgic.forge.network;
 
 import mod.adrenix.nostalgic.NostalgicTweaks;
+import mod.adrenix.nostalgic.server.config.ServerConfigCache;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraftforge.network.NetworkRegistry;
 
@@ -12,6 +13,19 @@ import net.minecraftforge.network.NetworkRegistry;
 public abstract class NostalgicNetwork
 {
     /**
+     * Checks if the given protocol version matches the sided protocol version.
+     * @param protocol A protocol version.
+     * @return Whether protocols match or <code>true</code> if SSO mode is enabled.
+     */
+    public static boolean isProtocolMatched(String protocol)
+    {
+        if (NostalgicTweaks.isServer() && ServerConfigCache.getRoot().serverSideOnlyMode)
+            return true;
+
+        return protocol.equals(NostalgicTweaks.PROTOCOL);
+    }
+
+    /**
      * Initializes a network channel builder with the mod's identifier and the mod's network protocol.
      * This is a simple network channel that enforces protocols match between the client and server.
      */
@@ -20,8 +34,8 @@ public abstract class NostalgicNetwork
         NetworkRegistry.ChannelBuilder
             .named(new ResourceLocation(NostalgicTweaks.MOD_ID, "nt_forge_verification"))
             .networkProtocolVersion(() -> NostalgicTweaks.PROTOCOL)
-            .clientAcceptedVersions(NostalgicTweaks.PROTOCOL::equals)
-            .serverAcceptedVersions(NostalgicTweaks.PROTOCOL::equals)
+            .clientAcceptedVersions(NostalgicNetwork::isProtocolMatched)
+            .serverAcceptedVersions(NostalgicNetwork::isProtocolMatched)
             .simpleChannel()
         ;
     }
