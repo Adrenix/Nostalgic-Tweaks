abd776be347a3bba856fb3cd219cffd95d24bab4
Adrenix <15348635+Adrenix@users.noreply.github.com>
2023-06-16 16:44:07 -0400

    Fix old armor damage tint not working on Forge

--- forge/src/main/java/mod/adrenix/nostalgic/forge/mixin/client/HumanoidArmorLayerForgeMixin.java
+++ forge/src/main/java/mod/adrenix/nostalgic/forge/mixin/client/HumanoidArmorLayerForgeMixin.java
@@ -10,6 +10,7 @@ import net.minecraft.client.renderer.entity.layers.HumanoidArmorLayer;
 import net.minecraft.client.renderer.texture.OverlayTexture;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.item.ArmorItem;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
@@ -17,7 +18,7 @@ import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(HumanoidArmorLayer.class)
-public abstract class HumanoidArmorLayerForgeMixin <T extends LivingEntity>
+public abstract class HumanoidArmorLayerForgeMixin<T extends LivingEntity>
 {
     /* Shadows & Unique */
 
@@ -28,7 +29,10 @@ public abstract class HumanoidArmorLayerForgeMixin <T extends LivingEntity>
     /**
      * Tracks the current living entity wearing this armor piece.
      */
-    @Inject(at = @At("HEAD"), method = "render(Lcom/mojang/blaze3d/vertex/PoseStack;Lnet/minecraft/client/renderer/MultiBufferSource;ILnet/minecraft/world/entity/LivingEntity;FFFFFF)V")
+    @Inject(
+        at = @At("HEAD"),
+        method = "render(Lcom/mojang/blaze3d/vertex/PoseStack;Lnet/minecraft/client/renderer/MultiBufferSource;ILnet/minecraft/world/entity/LivingEntity;FFFFFF)V"
+    )
     private void NT$onStartRender(PoseStack poseStack, MultiBufferSource buffer, int packedLight, T livingEntity, float limbSwing, float limbSwingAmount, float partialTicks, float ageInTicks, float netHeadYaw, float headPitch, CallbackInfo callback)
     {
         this.NT$armorWearer = livingEntity;
@@ -37,18 +41,26 @@ public abstract class HumanoidArmorLayerForgeMixin <T extends LivingEntity>
     /**
      * Removes tracking information.
      */
-    @Inject(at = @At("RETURN"), method = "render(Lcom/mojang/blaze3d/vertex/PoseStack;Lnet/minecraft/client/renderer/MultiBufferSource;ILnet/minecraft/world/entity/LivingEntity;FFFFFF)V")
+    @Inject(
+        at = @At("RETURN"),
+        method = "render(Lcom/mojang/blaze3d/vertex/PoseStack;Lnet/minecraft/client/renderer/MultiBufferSource;ILnet/minecraft/world/entity/LivingEntity;FFFFFF)V"
+    )
     private void NT$onFinishRender(PoseStack poseStack, MultiBufferSource buffer, int packedLight, T livingEntity, float limbSwing, float limbSwingAmount, float partialTicks, float ageInTicks, float netHeadYaw, float headPitch, CallbackInfo callback)
     {
         this.NT$armorWearer = null;
     }
 
     /**
-     * Changes the rendering of the armor piece depending on whether the tracked entity wearing this armor is hurt or dead.
-     * Controlled by the old damage armor tint tweak.
+     * Changes the rendering of the armor piece depending on whether the tracked entity wearing this armor is hurt or
+     * dead. Controlled by the old damage armor tint tweak.
      */
-    @Inject(at = @At("HEAD"), remap = false, cancellable = true, method = "renderModel(Lcom/mojang/blaze3d/vertex/PoseStack;Lnet/minecraft/client/renderer/MultiBufferSource;IZLnet/minecraft/client/model/Model;FFFLnet/minecraft/resources/ResourceLocation;)V")
-    private void NT$onRenderModel(PoseStack poseStack, MultiBufferSource buffer, int packedLight, boolean hasFoil, Model model, float r, float g, float b, ResourceLocation location, CallbackInfo callback)
+    @Inject(
+        remap = false,
+        cancellable = true,
+        at = @At("HEAD"),
+        method = "renderModel(Lcom/mojang/blaze3d/vertex/PoseStack;Lnet/minecraft/client/renderer/MultiBufferSource;ILnet/minecraft/world/item/ArmorItem;Lnet/minecraft/client/model/Model;ZFFFLnet/minecraft/resources/ResourceLocation;)V"
+    )
+    private void NT$onRenderModel(PoseStack poseStack, MultiBufferSource buffer, int packedLight, ArmorItem armorItem, Model model, boolean hasFoil, float r, float g, float b, ResourceLocation location, CallbackInfo callback)
     {
         boolean isHurt = ModConfig.Candy.oldDamageArmorTint() && (this.NT$armorWearer.hurtTime > 0 || this.NT$armorWearer.deathTime > 0);
 
