b24d538c4439d98dfe2e721bffcc09f59cf9bb64
Adrenix <15348635+Adrenix@users.noreply.github.com>
2023-06-16 17:48:24 -0400

    Fix unable to exit resource pack from title screen

--- common/src/main/java/mod/adrenix/nostalgic/client/screen/NostalgicTitleScreen.java
+++ common/src/main/java/mod/adrenix/nostalgic/client/screen/NostalgicTitleScreen.java
@@ -1,6 +1,5 @@
 package mod.adrenix.nostalgic.client.screen;
 
-import com.google.common.collect.ImmutableList;
 import com.mojang.blaze3d.platform.GlStateManager;
 import com.mojang.blaze3d.systems.RenderSystem;
 import com.mojang.blaze3d.vertex.*;
@@ -16,7 +15,6 @@ import mod.adrenix.nostalgic.util.common.TextureLocation;
 import net.minecraft.ChatFormatting;
 import net.minecraft.Util;
 import net.minecraft.client.Minecraft;
-import net.minecraft.client.Options;
 import net.minecraft.client.gui.components.AbstractWidget;
 import net.minecraft.client.gui.components.Button;
 import net.minecraft.client.gui.components.ImageButton;
@@ -34,7 +32,6 @@ import net.minecraft.client.renderer.PanoramaRenderer;
 import net.minecraft.client.resources.language.I18n;
 import net.minecraft.network.chat.Component;
 import net.minecraft.resources.ResourceLocation;
-import net.minecraft.server.packs.repository.Pack;
 import net.minecraft.server.packs.repository.PackRepository;
 import net.minecraft.util.Mth;
 import net.minecraft.util.RandomSource;
@@ -434,31 +431,8 @@ public class NostalgicTitleScreen extends TitleScreen
         if (this.minecraft == null)
             return;
 
-        Options options = this.minecraft.options;
-        ImmutableList<String> before = ImmutableList.copyOf(options.resourcePacks);
-
-        options.resourcePacks.clear();
-        options.incompatibleResourcePacks.clear();
-
-        for (Pack pack : repository.getSelectedPacks())
-        {
-            if (pack.isFixedPosition())
-                continue;
-
-            options.resourcePacks.add(pack.getId());
-
-            if (pack.getCompatibility().isCompatible())
-                continue;
-
-            options.incompatibleResourcePacks.add(pack.getId());
-        }
-
-        options.save();
-
-        ImmutableList<String> after = ImmutableList.copyOf(options.resourcePacks);
-
-        if (!after.equals(before))
-            this.minecraft.reloadResourcePacks();
+        this.minecraft.options.updateResourcePacks(repository);
+        this.minecraft.setScreen(this);
     }
 
     /**
