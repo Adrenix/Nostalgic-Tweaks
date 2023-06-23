555fca2bda011d7837bcfa5087709fa491769efe
Adrenix <15348635+Adrenix@users.noreply.github.com>
2023-06-23 16:10:47 -0400

    Updated mod tracker and its mod loader initialization

--- forge/src/main/java/mod/adrenix/nostalgic/forge/register/CommonSetup.java
+++ forge/src/main/java/mod/adrenix/nostalgic/forge/register/CommonSetup.java
@@ -2,7 +2,9 @@ package mod.adrenix.nostalgic.forge.register;
 
 import mod.adrenix.nostalgic.NostalgicTweaks;
 import mod.adrenix.nostalgic.forge.network.NostalgicNetwork;
+import mod.adrenix.nostalgic.util.ModTracker;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.ModList;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 
@@ -15,9 +17,16 @@ import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 public abstract class CommonSetup
 {
     /**
-     * Initializes the network channel so that network protocol matching is enforced.
+     * Initializes mod tracking and the network channel so that network protocol matching is enforced.
      * @param event A FML common setup event instance.
      */
     @SubscribeEvent
-    public static void init(FMLCommonSetupEvent event) { NostalgicNetwork.init(); }
+    public static void init(FMLCommonSetupEvent event)
+    {
+        // Mod Tacking
+        ModTracker.init(ModList.get()::isLoaded);
+
+        // Network
+        NostalgicNetwork.init();
+    }
 }
