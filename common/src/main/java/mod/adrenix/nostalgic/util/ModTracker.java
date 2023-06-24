440751464bdb91c5822f59d3b5d58d551889a8ed
Adrenix <15348635+Adrenix@users.noreply.github.com>
2023-06-24 17:21:19 -0400

    Updated mod tracker and its mod loader initialization

--- common/src/main/java/mod/adrenix/nostalgic/util/ModTracker.java
+++ common/src/main/java/mod/adrenix/nostalgic/util/ModTracker.java
@@ -3,13 +3,16 @@ package mod.adrenix.nostalgic.util;
 import com.google.common.base.Suppliers;
 import mod.adrenix.nostalgic.util.common.ClassUtil;
 
+import java.util.Arrays;
 import java.util.function.Function;
 import java.util.function.Supplier;
+import java.util.stream.Stream;
 
 public enum ModTracker
 {
     APPLE_SKIN("appleskin"),
     SODIUM("sodium"),
+    EXORDIUM("exordium"),
     OPTIFINE("optifine"),
     FLYWHEEL("flywheel");
 
@@ -20,29 +23,47 @@ public enum ModTracker
 
     /* Constructor */
 
-    ModTracker(String id) { this.id = id; }
+    ModTracker(String id)
+    {
+        this.id = id;
+    }
 
     /* Methods */
 
     /**
-     * This field tracks whether Optifine is installed. This is a Forge mod, but has a Fabric relative known as Opti-
-     * Fabric. A supplier is needed since a hacky class check is required to determine if Optifine is present. This
+     * This field tracks whether Optifine is installed. This is a Forge mod, but has a Fabric relative known as
+     * Opti-Fabric. A supplier is needed since a hacky class check is required to determine if Optifine is present. This
      * hacky check is needed because Optifine is a closed-source mod.
      */
     private static final Supplier<Boolean> OPTIFINE_SUPPLIER = Suppliers.memoize(ClassUtil::isOptifinePresent);
 
     /**
-     * Checks if the mod is installed using an applicable mod loader function.
+     * Get a stream of all mod tracker values.
+     *
+     * @return A {@link Stream} of this enumeration's values.
+     */
+    public static Stream<ModTracker> stream()
+    {
+        return Arrays.stream(ModTracker.values());
+    }
+
+    /**
+     * Initialize the mod tracker utility using the given mod loader function. Each enumeration will have its
+     * {@code installed} flag updated based on the value returned by the given {@code loader} {@link Function}.
+     *
      * @param loader A function that accepts a mod id and returns whether the mod id was found in the mod folder.
      */
-    public void load(Function<String, Boolean> loader) { this.installed = loader.apply(this.id); }
+    public static void init(Function<String, Boolean> loader)
+    {
+        ModTracker.stream().forEach(mod -> mod.installed = loader.apply(mod.id));
+    }
 
     /**
      * Checks if the mod is installed. This will not return a truthful value if the mod scanning process has not yet
      * occurred.
-     *
-     * The Optifine mod needs a special of way of checking if it is installed. This may not always return a correct
-     * value if the class that is checked is changed by Optifine in the future.
+     * <br><br>
+     * The Optifine mod needs a special way of checking if it is installed. This may not always return a correct value
+     * if the class that is checked is changed by Optifine in the future.
      *
      * @return Whether the mod is installed.
      */
