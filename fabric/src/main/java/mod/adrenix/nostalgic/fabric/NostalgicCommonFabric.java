de4ac4834529db99688e66156bf9ee08b02c7b1c
Adrenix <15348635+Adrenix@users.noreply.github.com>
2023-07-10 14:08:37 -0400

    (8.4 Backport) Updated mod tracker and its mod loader initialization

--- fabric/src/main/java/mod/adrenix/nostalgic/fabric/NostalgicCommonFabric.java
+++ fabric/src/main/java/mod/adrenix/nostalgic/fabric/NostalgicCommonFabric.java
@@ -2,7 +2,9 @@ package mod.adrenix.nostalgic.fabric;
 
 import mod.adrenix.nostalgic.NostalgicTweaks;
 import mod.adrenix.nostalgic.fabric.event.CommonEvents;
+import mod.adrenix.nostalgic.util.ModTracker;
 import net.fabricmc.api.ModInitializer;
+import net.fabricmc.loader.api.FabricLoader;
 import net.minecraft.resources.ResourceLocation;
 
 /**
@@ -24,6 +26,9 @@ public class NostalgicCommonFabric implements ModInitializer
     @Override
     public void onInitialize()
     {
+        // Mod tracking
+        ModTracker.init(FabricLoader.getInstance()::isModLoaded);
+
         // Register common
         CommonEvents.register();
     }
