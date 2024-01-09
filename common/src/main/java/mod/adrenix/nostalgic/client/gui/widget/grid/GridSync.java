29f29db5bc370d4d2f1cd6bb32feb048d0149537
Adrenix <15348635+Adrenix@users.noreply.github.com>
2024-01-09 00:11:01 -0500

    Fixed grid realignment issues

--- common/src/main/java/mod/adrenix/nostalgic/client/gui/widget/grid/GridSync.java
+++ common/src/main/java/mod/adrenix/nostalgic/client/gui/widget/grid/GridSync.java
@@ -5,11 +5,14 @@ import mod.adrenix.nostalgic.client.gui.widget.dynamic.DynamicFunction;
 import mod.adrenix.nostalgic.client.gui.widget.dynamic.DynamicPriority;
 import mod.adrenix.nostalgic.client.gui.widget.dynamic.WidgetCache;
 import mod.adrenix.nostalgic.util.common.data.CacheHolder;
+import mod.adrenix.nostalgic.util.common.data.RecursionAvoidance;
 
 import java.util.List;
 
 public class GridSync implements DynamicFunction<GridBuilder, Grid>
 {
+    private final RecursionAvoidance aligner = RecursionAvoidance.create();
+
     @Override
     public void apply(Grid grid, GridBuilder builder)
     {
@@ -17,11 +20,16 @@ public class GridSync implements DynamicFunction<GridBuilder, Grid>
 
         if (grid.isRealignNeeded)
             grid.realign();
+
+        this.aligner.process(builder::sync);
     }
 
     @Override
     public boolean isReapplyNeeded(Grid grid, GridBuilder builder, WidgetCache cache)
     {
+        if (this.aligner.isProcessing())
+            return false;
+
         if (grid.cellsPerRow != builder.cellsPerRow.applyAsInt(grid))
             return true;
 
