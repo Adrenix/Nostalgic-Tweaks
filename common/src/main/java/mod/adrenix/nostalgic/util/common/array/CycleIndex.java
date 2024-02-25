1beb71f46fc78391e8d2cc8c50fc48abd8bce2cd
Adrenix <15348635+Adrenix@users.noreply.github.com>
2024-02-25 13:36:41 -0500

    Added reset flag to CycleIndex util

--- common/src/main/java/mod/adrenix/nostalgic/util/common/array/CycleIndex.java
+++ common/src/main/java/mod/adrenix/nostalgic/util/common/array/CycleIndex.java
@@ -12,6 +12,7 @@ public class CycleIndex
     private int lastIndex;
     private int currentIndex;
     private boolean movingForward;
+    private boolean resetAtEnd;
 
     /* Constructor */
 
@@ -21,14 +22,29 @@ public class CycleIndex
      * @param minIndex   The minimum index.
      * @param maxIndex   The maximum index.
      * @param startIndex The starting index (will be clamped).
+     * @param resetAtEnd Reset the cycle back to the minimum index when finished.
      */
-    public CycleIndex(int minIndex, int maxIndex, int startIndex)
+    public CycleIndex(int minIndex, int maxIndex, int startIndex, boolean resetAtEnd)
     {
         this.minIndex = minIndex;
         this.maxIndex = maxIndex;
         this.currentIndex = Mth.clamp(startIndex, minIndex, maxIndex);
         this.lastIndex = this.currentIndex;
+        this.resetAtEnd = resetAtEnd;
         this.movingForward = true;
+    }
+
+    /**
+     * Create a new {@link CycleIndex} instance using an array where the {@code minIndex} is zero and the
+     * {@code maxIndex} is the length of the array minus one.
+     *
+     * @param array      An array.
+     * @param resetAtEnd Reset the cycle back to the minimum index when finished.
+     * @param <T>        The class type of the values in the array.
+     */
+    public <T> CycleIndex(T[] array, boolean resetAtEnd)
+    {
+        this(0, array.length - 1, 0, resetAtEnd);
     }
 
     /**
@@ -40,7 +56,7 @@ public class CycleIndex
      */
     public <T> CycleIndex(T[] array)
     {
-        this(0, array.length - 1, 0);
+        this(array, false);
     }
 
     /* Methods */
@@ -58,7 +74,11 @@ public class CycleIndex
         if (nextIndex > this.maxIndex)
         {
             this.movingForward = false;
-            this.currentIndex--;
+
+            if (this.resetAtEnd)
+                this.currentIndex = 0;
+            else
+                this.currentIndex--;
 
             return;
         }
@@ -100,5 +120,16 @@ public class CycleIndex
     {
         this.cycle();
         return this.get();
+    }
+
+    /**
+     * Set whether the cycle restarts when it is finished.
+     *
+     * @param state The new flag state.
+     */
+    @PublicAPI
+    public void setResetAtEnd(boolean state)
+    {
+        this.resetAtEnd = state;
     }
 }
