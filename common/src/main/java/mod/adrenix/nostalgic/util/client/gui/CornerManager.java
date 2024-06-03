4d491d2518242878f46e8a4b6fc889e05c61451e
Adrenix <15348635+Adrenix@users.noreply.github.com>
2024-06-03 17:30:56 -0400

    Fixed effect status icons overlapping top-right text

--- common/src/main/java/mod/adrenix/nostalgic/util/client/gui/CornerManager.java
+++ common/src/main/java/mod/adrenix/nostalgic/util/client/gui/CornerManager.java
@@ -1,8 +1,15 @@
 package mod.adrenix.nostalgic.util.client.gui;
 
-import com.google.common.util.concurrent.AtomicDouble;
 import mod.adrenix.nostalgic.tweak.enums.Corner;
+import mod.adrenix.nostalgic.util.common.data.NullableResult;
+import mod.adrenix.nostalgic.util.common.data.NumberHolder;
+import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.GuiGraphics;
+import net.minecraft.client.player.LocalPlayer;
+import net.minecraft.world.effect.MobEffectInstance;
+
+import java.util.Collection;
+import java.util.HashSet;
 
 /**
  * This utility manages and keeps track of where the overlay text should be rendered on the HUD. Use this utility when
@@ -11,10 +18,10 @@ import net.minecraft.client.gui.GuiGraphics;
 public class CornerManager
 {
     private final float height = (float) GuiUtil.getGuiHeight();
-    private final AtomicDouble topLeft = new AtomicDouble(2.0D);
-    private final AtomicDouble topRight = new AtomicDouble(2.0D);
-    private final AtomicDouble bottomLeft = new AtomicDouble(this.height - 10.0D);
-    private final AtomicDouble bottomRight = new AtomicDouble(this.height - 10.0D);
+    private final NumberHolder<Double> topLeft = NumberHolder.create(2.0D);
+    private final NumberHolder<Double> topRight = NumberHolder.create(2.0D);
+    private final NumberHolder<Double> bottomLeft = NumberHolder.create(this.height - 10.0D);
+    private final NumberHolder<Double> bottomRight = NumberHolder.create(this.height - 10.0D);
 
     /**
      * Get where the next line for the given corner should be drawn and then add 10 units to the corner offset.
@@ -53,8 +60,19 @@ public class CornerManager
      */
     public void drawText(GuiGraphics graphics, String text, Corner corner)
     {
-        DrawText.begin(graphics, text)
-            .pos(corner.isLeft() ? 2 : this.getRightOffset(text), (int) this.getAndAdd(corner))
-            .draw();
+        int x = corner.isLeft() ? 2 : this.getRightOffset(text);
+        int y = (int) this.getAndAdd(corner);
+        int effectOffset = 0;
+
+        if (corner == Corner.TOP_RIGHT)
+        {
+            LocalPlayer player = Minecraft.getInstance().player;
+            Collection<MobEffectInstance> effects = NullableResult.getOrElse(player, new HashSet<>(), LocalPlayer::getActiveEffects);
+
+            if (!effects.isEmpty())
+                effectOffset = 25;
+        }
+
+        DrawText.begin(graphics, text).pos(x, y + effectOffset).draw();
     }
 }
