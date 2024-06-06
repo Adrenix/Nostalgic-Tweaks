8a5e0f16ca3cac8b194a4ba141dbd95b3fea7c82
Adrenix <15348635+Adrenix@users.noreply.github.com>
2024-06-06 15:31:08 -0400

    Fixed reequip animation playing when using instant bows

--- common/src/main/java/mod/adrenix/nostalgic/mixin/tweak/gameplay/combat_bow/MinecraftMixin.java
+++ common/src/main/java/mod/adrenix/nostalgic/mixin/tweak/gameplay/combat_bow/MinecraftMixin.java
@@ -4,14 +4,23 @@ import com.llamalad7.mixinextras.injector.WrapWithCondition;
 import mod.adrenix.nostalgic.tweak.config.GameplayTweak;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.player.LocalPlayer;
+import net.minecraft.client.renderer.ItemInHandRenderer;
 import net.minecraft.world.InteractionHand;
 import net.minecraft.world.item.Items;
+import org.jetbrains.annotations.Nullable;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 
 @Mixin(Minecraft.class)
 public abstract class MinecraftMixin
 {
+    /* Shadows */
+
+    @Shadow @Nullable public LocalPlayer player;
+
+    /* Injections */
+
     /**
      * Prevents the player from swinging their interaction hand on the client when using the instantaneous bow tweak.
      */
@@ -26,5 +35,24 @@ public abstract class MinecraftMixin
     private boolean nt_combat_bow$shouldSwingOnBowUse(LocalPlayer player, InteractionHand hand)
     {
         return !GameplayTweak.INSTANT_BOW.get() || !player.getItemInHand(hand).getItem().equals(Items.BOW);
+    }
+
+    /**
+     * Prevents the reequipped animation after a bow fires when using the instantaneous bow tweak.
+     */
+    @WrapWithCondition(
+        method = "startUseItem",
+        at = @At(
+            ordinal = 1,
+            value = "INVOKE",
+            target = "Lnet/minecraft/client/renderer/ItemInHandRenderer;itemUsed(Lnet/minecraft/world/InteractionHand;)V"
+        )
+    )
+    private boolean nt_combat_bow$shouldSetItemAsUsed(ItemInHandRenderer itemInHandRenderer, InteractionHand hand)
+    {
+        if (this.player == null)
+            return true;
+
+        return !GameplayTweak.INSTANT_BOW.get() || !this.player.getItemInHand(hand).getItem().equals(Items.BOW);
     }
 }
--- common/src/main/java/mod/adrenix/nostalgic/mixin/tweak/swing/ItemInHandRendererMixin.java
+++ common/src/main/java/mod/adrenix/nostalgic/mixin/tweak/swing/ItemInHandRendererMixin.java
@@ -1,15 +1,8 @@
 package mod.adrenix.nostalgic.mixin.tweak.swing;
 
-import com.mojang.blaze3d.vertex.PoseStack;
-import mod.adrenix.nostalgic.tweak.config.GameplayTweak;
 import mod.adrenix.nostalgic.tweak.config.ModTweak;
 import mod.adrenix.nostalgic.tweak.config.SwingTweak;
-import net.minecraft.client.player.AbstractClientPlayer;
 import net.minecraft.client.renderer.ItemInHandRenderer;
-import net.minecraft.client.renderer.MultiBufferSource;
-import net.minecraft.world.InteractionHand;
-import net.minecraft.world.item.ItemStack;
-import net.minecraft.world.item.Items;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.ModifyArg;
@@ -19,8 +12,7 @@ public abstract class ItemInHandRendererMixin
 {
     /**
      * Enhances photosensitivity mode by completely disabling any hand movement when placing or interacting. Only checks
-     * for global photosensitivity since this will break reequip animations if checking by item. Any item that needs to
-     * disable their reequip progress, such as instantaneous bows, is done here.
+     * for global photosensitivity since this will break reequip animations if checking by item.
      */
     @ModifyArg(
         index = 6,
@@ -30,13 +22,10 @@ public abstract class ItemInHandRendererMixin
             target = "Lnet/minecraft/client/renderer/ItemInHandRenderer;renderArmWithItem(Lnet/minecraft/client/player/AbstractClientPlayer;FFLnet/minecraft/world/InteractionHand;FLnet/minecraft/world/item/ItemStack;FLcom/mojang/blaze3d/vertex/PoseStack;Lnet/minecraft/client/renderer/MultiBufferSource;I)V"
         )
     )
-    private float nt_swing$modifyEquippedProgress(AbstractClientPlayer player, float partialTicks, float pitch, InteractionHand hand, float swingProgress, ItemStack itemStack, float equippedProgress, PoseStack poseStack, MultiBufferSource buffer, int combinedLight)
+    private float nt_swing$modifyEquippedProgress(float equippedProgress)
     {
         if (!ModTweak.ENABLED.get())
             return equippedProgress;
-
-        if (GameplayTweak.INSTANT_BOW.get() && itemStack.getItem().equals(Items.BOW))
-            return 0.0F;
 
         return SwingTweak.USE_GLOBAL_SPEED.get() == SwingTweak.PHOTOSENSITIVE ? 0.0F : equippedProgress;
     }
