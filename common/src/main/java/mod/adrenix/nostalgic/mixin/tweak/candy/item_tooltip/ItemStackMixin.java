68a20c240b95f2d5324d921c8c4aa9de89ab8e34
Adrenix <15348635+Adrenix@users.noreply.github.com>
2024-06-19 15:19:35 -0400

    Fixed hide item modifiers tooltip tweak

--- common/src/main/java/mod/adrenix/nostalgic/mixin/tweak/candy/item_tooltip/ItemStackMixin.java
+++ common/src/main/java/mod/adrenix/nostalgic/mixin/tweak/candy/item_tooltip/ItemStackMixin.java
@@ -6,17 +6,17 @@ import net.minecraft.core.Holder;
 import net.minecraft.core.component.DataComponentType;
 import net.minecraft.core.component.DataComponents;
 import net.minecraft.network.chat.Component;
+import net.minecraft.world.entity.EquipmentSlot;
 import net.minecraft.world.entity.ai.attributes.Attribute;
 import net.minecraft.world.entity.ai.attributes.AttributeModifier;
-import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.TooltipFlag;
 import net.minecraft.world.item.component.TooltipProvider;
-import org.jetbrains.annotations.Nullable;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 
+import java.util.function.BiConsumer;
 import java.util.function.Consumer;
 
 @Mixin(ItemStack.class)
@@ -46,13 +46,13 @@ public abstract class ItemStackMixin
      * Determines whether the modifiers are shown on an item tooltip.
      */
     @WrapWithCondition(
-        method = "method_57370",
+        method = "addAttributeTooltips",
         at = @At(
             value = "INVOKE",
-            target = "Lnet/minecraft/world/item/ItemStack;addModifierTooltip(Ljava/util/function/Consumer;Lnet/minecraft/world/entity/player/Player;Lnet/minecraft/core/Holder;Lnet/minecraft/world/entity/ai/attributes/AttributeModifier;)V"
+            target = "Lnet/minecraft/world/item/ItemStack;forEachModifier(Lnet/minecraft/world/entity/EquipmentSlot;Ljava/util/function/BiConsumer;)V"
         )
     )
-    private boolean nt_item_tooltip$shouldAddModifierTooltip(ItemStack itemStack, Consumer<Component> tooltipAdder, @Nullable Player player, Holder<Attribute> attribute, AttributeModifier modifier)
+    private boolean nt_item_tooltip$shouldAddModifierTooltip(ItemStack itemStack, EquipmentSlot equipmentSLot, BiConsumer<Holder<Attribute>, AttributeModifier> action)
     {
         return CandyTweak.SHOW_MODIFIER_TIP.get();
     }
