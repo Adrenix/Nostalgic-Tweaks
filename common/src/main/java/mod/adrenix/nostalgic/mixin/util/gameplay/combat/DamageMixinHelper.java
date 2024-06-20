eda6ae9574f6e4fd04f18afc71b985d37f2b36ca
Adrenix <15348635+Adrenix@users.noreply.github.com>
2024-06-20 17:32:58 -0400

    Rewrote old damage value tweak for 1.20.6

--- common/src/main/java/mod/adrenix/nostalgic/mixin/util/gameplay/combat/DamageMixinHelper.java
+++ common/src/main/java/mod/adrenix/nostalgic/mixin/util/gameplay/combat/DamageMixinHelper.java
@@ -1,9 +1,13 @@
 package mod.adrenix.nostalgic.mixin.util.gameplay.combat;
 
+import net.minecraft.world.entity.EquipmentSlotGroup;
+import net.minecraft.world.entity.ai.attributes.AttributeModifier;
+import net.minecraft.world.entity.ai.attributes.Attributes;
 import net.minecraft.world.item.*;
+import net.minecraft.world.item.component.ItemAttributeModifiers;
 
+import java.util.HashMap;
 import java.util.Map;
-import java.util.function.Function;
 
 /**
  * This utility class is used by both the client and server.
@@ -11,42 +15,74 @@ import java.util.function.Function;
 public abstract class DamageMixinHelper
 {
     /**
-     * A cache map that contains old tiered damage values.
+     * A cache map that connects an item to its old damage value item attribute.
      */
-    // @formatter:off
-    private static final Map<Class<? extends TieredItem>, Function<TieredItem, Float>> OLD_DAMAGE_MAP = Map.of(
-        SwordItem.class, item -> item.getTier().getAttackDamageBonus() + 4.0F,
-        AxeItem.class, item -> item.getTier().getAttackDamageBonus() + 3.0F,
-        PickaxeItem.class, item -> item.getTier().getAttackDamageBonus() + 2.0F,
-        ShovelItem.class, item -> item.getTier().getAttackDamageBonus() + 1.0F,
-        HoeItem.class, item -> item.getTier().getAttackDamageBonus()
-    );
-    // @formatter:on
+    private static final Map<String, ItemAttributeModifiers> DAMAGE_MODIFIERS = new HashMap<>();
 
     /**
-     * Check if the given item has a tier and has an old value.
+     * Get the attack damage amount for a tiered item.
      *
-     * @param item The {@link TieredItem} to check.
-     * @return Whether the given item has an old damage value.
+     * @param tieredItem The {@link TieredItem} instance.
+     * @return The attack damage amount.
      */
-    public static boolean isApplicable(TieredItem item)
+    private static float getAttackDamage(TieredItem tieredItem)
     {
-        return OLD_DAMAGE_MAP.containsKey(item.getClass());
+        return switch (tieredItem)
+        {
+            case SwordItem sword -> sword.getTier().getAttackDamageBonus() + 4.0F;
+            case AxeItem axe -> axe.getTier().getAttackDamageBonus() + 3.0F;
+            case PickaxeItem pickaxe -> pickaxe.getTier().getAttackDamageBonus() + 2.0F;
+            case ShovelItem shovel -> shovel.getTier().getAttackDamageBonus() + 1.0F;
+            default -> tieredItem.getTier().getAttackDamageBonus();
+        };
     }
 
     /**
-     * Get the old damage value based on the provided item.
+     * Check if the given tiered item has an old value.
      *
-     * @param item The {@link TieredItem} instance.
-     * @return An old damage value, if it is a vanilla item.
+     * @param tieredItem The {@link TieredItem} to check.
+     * @return Whether the given tiered item has an old damage value.
      */
-    public static float get(TieredItem item)
+    public static boolean isApplicable(TieredItem tieredItem)
     {
-        Class<? extends TieredItem> tierClass = item.getClass();
+        return switch (tieredItem)
+        {
+            case SwordItem ignored -> true;
+            case AxeItem ignored -> true;
+            case PickaxeItem ignored -> true;
+            case ShovelItem ignored -> true;
+            case HoeItem ignored -> true;
+            default -> false;
+        };
+    }
 
-        if (OLD_DAMAGE_MAP.containsKey(tierClass))
-            return OLD_DAMAGE_MAP.get(tierClass).apply(item);
+    /**
+     * Get the old damage value based on the provided tiered item.
+     *
+     * @param tieredItem The {@link TieredItem} instance.
+     * @param attributes The {@link ItemAttributeModifiers} instance.
+     * @return An old damage value, if it is a vaild item.
+     */
+    public static ItemAttributeModifiers get(final TieredItem tieredItem, final ItemAttributeModifiers attributes)
+    {
+        return DAMAGE_MODIFIERS.computeIfAbsent(tieredItem.getDescriptionId(), itemClass -> {
+            ItemAttributeModifiers.Builder builder = ItemAttributeModifiers.builder();
 
-        return 0.0F;
+            attributes.modifiers().forEach(entry -> {
+                if (entry.attribute() != Attributes.ATTACK_DAMAGE)
+                    builder.add(entry.attribute(), entry.modifier(), entry.slot());
+                else
+                {
+                    if (tieredItem instanceof DiggerItem diggerItem)
+                        builder.add(Attributes.ATTACK_DAMAGE, new AttributeModifier(Item.BASE_ATTACK_DAMAGE_UUID, "Tool modifier", getAttackDamage(diggerItem), AttributeModifier.Operation.ADD_VALUE), EquipmentSlotGroup.MAINHAND);
+                    else if (tieredItem instanceof SwordItem swordItem)
+                        builder.add(Attributes.ATTACK_DAMAGE, new AttributeModifier(Item.BASE_ATTACK_DAMAGE_UUID, "Weapon modifier", getAttackDamage(swordItem), AttributeModifier.Operation.ADD_VALUE), EquipmentSlotGroup.MAINHAND);
+                    else
+                        builder.add(entry.attribute(), entry.modifier(), entry.slot());
+                }
+            });
+
+            return builder.build();
+        });
     }
 }
