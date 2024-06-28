6bcafbdb35b5afc8fe980d31a960039824753a28
Adrenix <15348635+Adrenix@users.noreply.github.com>
2024-06-28 15:30:40 -0400

    Added realtime partial tick getter

--- common/src/main/java/mod/adrenix/nostalgic/util/client/timer/PartialTick.java
+++ common/src/main/java/mod/adrenix/nostalgic/util/client/timer/PartialTick.java
@@ -5,10 +5,18 @@ import net.minecraft.client.Minecraft;
 public abstract class PartialTick
 {
     /**
-     * @return The normalized progress between two ticks [0.0F, 1.0F].
+     * @return Framerate based normalized progress between two ticks [0.0F, 1.0F].
      */
     public static float get()
     {
         return Minecraft.getInstance().getTimer().getGameTimeDeltaPartialTick(false);
+    }
+
+    /**
+     * @return Realtime based normalized progress between two ticks [0.0F, 1.0F].
+     */
+    public static float realtime()
+    {
+        return Minecraft.getInstance().getTimer().getRealtimeDeltaTicks();
     }
 }
