6ba99a112d36fb03c07bc0bc275c0aefda67db56
Adrenix <15348635+Adrenix@users.noreply.github.com>
2024-06-29 16:28:31 -0400

    Added disable animal breeding tweak

--- common/src/main/java/mod/adrenix/nostalgic/config/ClientConfig.java
+++ common/src/main/java/mod/adrenix/nostalgic/config/ClientConfig.java
@@ -663,6 +663,7 @@ public class ClientConfig implements ConfigMeta
         public int animalSpawnCap = GameplayTweak.ANIMAL_SPAWN_CAP.register("animalSpawnCap");
         public boolean oldAnimalSpawning = GameplayTweak.OLD_ANIMAL_SPAWNING.register("oldAnimalSpawning");
         public boolean disableBabyAnimalSpawning = GameplayTweak.DISABLE_BABY_ANIMAL_SPAWNING.register("disableBabyAnimalSpawning");
+        public boolean disableAnimalBreeding = GameplayTweak.DISABLE_ANIMAL_BREEDING.register("disableAnimalBreeding");
 
         // Sheep
 
