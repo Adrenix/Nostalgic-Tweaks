55e8da98080e1a6c01fd925e7c47c5a7751d19eb
Adrenix <15348635+Adrenix@users.noreply.github.com>
2024-07-03 13:40:56 -0400

    Added more ticking helpers to PartialTick utility

--- common/src/main/java/mod/adrenix/nostalgic/util/client/timer/PartialTick.java
+++ common/src/main/java/mod/adrenix/nostalgic/util/client/timer/PartialTick.java
@@ -1,20 +1,51 @@
 package mod.adrenix.nostalgic.util.client.timer;
 
+import mod.adrenix.nostalgic.mixin.access.TimerAccess;
+import mod.adrenix.nostalgic.util.common.annotation.PublicAPI;
 import net.minecraft.client.Minecraft;
 
 public abstract class PartialTick
 {
     /**
+     * This will be 1.0F if the level tick rate manager is frozen or is trying to catch up. This will also use the delta
+     * tick residual if the game is paused. Use {@link #normal()} if the frozen effect should be ignored. Use
+     * {@link #legacy()} if both of these effects are not desirable.
+     *
      * @return Framerate based normalized progress between two ticks [0.0F, 1.0F].
      */
+    @PublicAPI
     public static float get()
     {
         return Minecraft.getInstance().getTimer().getGameTimeDeltaPartialTick(false);
     }
 
     /**
+     * This will ignore if the level tick rate manager is frozen or is trying to catch up. This will use the delta tick
+     * residual if the game is paused. Use {@link #legacy()} if this effect is not desirable.
+     *
+     * @return Framerate based normalized progress between two ticks [0.0F, 1.0F].
+     */
+    @PublicAPI
+    public static float normal()
+    {
+        return Minecraft.getInstance().getTimer().getGameTimeDeltaPartialTick(true);
+    }
+
+    /**
+     * Get the legacy partial tick tracker that ignores if the game is frozen/paused.
+     *
+     * @return Framerate based normalized progress between two ticks [0.0F, 1.0F].
+     */
+    @PublicAPI
+    public static float legacy()
+    {
+        return ((TimerAccess) Minecraft.getInstance().getTimer()).nt$getPartialTick();
+    }
+
+    /**
      * @return Realtime based normalized progress between two ticks [0.0F, 1.0F].
      */
+    @PublicAPI
     public static float realtime()
     {
         return Minecraft.getInstance().getTimer().getRealtimeDeltaTicks();
