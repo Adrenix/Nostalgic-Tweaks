0a98e28c2ebf8389d84c724cfd20379fcaa2329b
Adrenix <15348635+Adrenix@users.noreply.github.com>
2024-08-06 15:02:10 -0400

    Fixed Remove Mipmaps tweak being used too early

--- common/src/main/java/mod/adrenix/nostalgic/mixin/tweak/candy/mip_map/ModelManagerMixin.java
+++ common/src/main/java/mod/adrenix/nostalgic/mixin/tweak/candy/mip_map/ModelManagerMixin.java
@@ -1,25 +1,55 @@
 package mod.adrenix.nostalgic.mixin.tweak.candy.mip_map;
 
 import mod.adrenix.nostalgic.tweak.config.CandyTweak;
+import net.minecraft.client.Minecraft;
 import net.minecraft.client.resources.model.ModelManager;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.ModifyVariable;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+import java.util.concurrent.CompletableFuture;
 
 @Mixin(ModelManager.class)
 public abstract class ModelManagerMixin
 {
+    /* Shadows */
+
+    @Shadow private int maxMipmapLevels;
+
+    /* Injections */
+
     /**
-     * Prevents changing of the mip level when changed by the video settings.
+     * Defines the instructions to perform when the mipmap tweak is changed.
      */
-    @ModifyVariable(
-        method = "updateMaxMipLevel",
-        argsOnly = true,
-        ordinal = 0,
+    @Inject(
+        method = "<init>",
+        at = @At("RETURN")
+    )
+    private void nt_mip_map$onInit(CallbackInfo callback)
+    {
+        CandyTweak.REMOVE_MIPMAP_TEXTURE.whenChanged(() -> {
+            if (CandyTweak.REMOVE_MIPMAP_TEXTURE.get())
+                this.maxMipmapLevels = 0;
+            else
+                this.maxMipmapLevels = Minecraft.getInstance().options.mipmapLevels().get();
+        });
+    }
+
+    /**
+     * Updates the mipmap level when the model manager is reloaded.
+     */
+    @Inject(
+        method = "reload",
         at = @At("HEAD")
     )
-    private int nt_mip_map$onSetMaxMipLevel(int value)
+    private void nt_mip_map$onReload(CallbackInfoReturnable<CompletableFuture<Void>> callback)
     {
-        return CandyTweak.REMOVE_MIPMAP_TEXTURE.get() ? 0 : value;
+        if (CandyTweak.REMOVE_MIPMAP_TEXTURE.get())
+            this.maxMipmapLevels = 0;
+        else
+            this.maxMipmapLevels = Minecraft.getInstance().options.mipmapLevels().get();
     }
 }
