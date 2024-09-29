2f9970d9c9c78519911fe050fa04be3bd15c6511
Xenthio <thegooster88@gmail.com>
2024-09-30 09:23:07 +1000

    do the blur better, scale with screen size too

--- common/src/main/java/mod/adrenix/nostalgic/mixin/tweak/candy/gui_background/PanoramaRendererMixin.java
+++ common/src/main/java/mod/adrenix/nostalgic/mixin/tweak/candy/gui_background/PanoramaRendererMixin.java
@@ -6,8 +6,13 @@ import mod.adrenix.nostalgic.util.client.timer.PartialTick;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.GuiGraphics;
 import net.minecraft.client.renderer.CubeMap;
+import net.minecraft.client.renderer.GameRenderer;
 import net.minecraft.client.renderer.PanoramaRenderer;
+import net.minecraft.client.renderer.PostChain;
+import org.jetbrains.annotations.Nullable;
+import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.ModifyArg;
@@ -16,6 +21,7 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 @Mixin(PanoramaRenderer.class)
 public abstract class PanoramaRendererMixin
 {
+    @Shadow @Final private Minecraft minecraft;
     private float time;
     @ModifyArg(
             method = "render",
@@ -39,11 +45,12 @@ public abstract class PanoramaRendererMixin
     )
     private void nt_panorama_renderer$DoBlurAndGradient(GuiGraphics guiGraphics, int width, int height, float fade, float partialTick, CallbackInfo ci) {
         if (CandyTweak.OLD_PANORAMA_BLUR.get()) {
-            // Seemingly no way to control the blur, so just do it four times, cant hurt performance too much + we get to reuse minecraft code instead of inventing our own
-            Minecraft.getInstance().gameRenderer.processBlurEffect(partialTick);
-            Minecraft.getInstance().gameRenderer.processBlurEffect(partialTick);
-            Minecraft.getInstance().gameRenderer.processBlurEffect(partialTick);
-            Minecraft.getInstance().gameRenderer.processBlurEffect(partialTick);
+            if (minecraft.gameRenderer.blurEffect != null) {
+                // This doesn't scale with screen size by default.
+                float scale = ((float)minecraft.getWindow().getHeight() / 480f);
+                minecraft.gameRenderer.blurEffect.setUniform("Radius", 5f * scale);
+                minecraft.gameRenderer.blurEffect.process(partialTick);
+            }
             Minecraft.getInstance().getMainRenderTarget().bindWrite(false);
             guiGraphics.fillGradient(0, 0, width, height, -2130706433, 16777215);
             guiGraphics.fillGradient(0, 0, width, height, 0, Integer.MIN_VALUE);
