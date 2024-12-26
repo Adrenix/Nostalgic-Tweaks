5cc6be3c16eec8b54ceaca10814c234649897761
Adrenix <15348635+adrenix@users.noreply.github.com>
2024-12-26 01:22:38 -0500

    Added hide advancement chat messages tweak

---
+++ common/src/main/java/mod/adrenix/nostalgic/mixin/tweak/candy/old_hud/PlayerAdvancementsMixin.java
@@ -0,0 +1,26 @@
+package mod.adrenix.nostalgic.mixin.tweak.candy.old_hud;
+
+import com.llamalad7.mixinextras.injector.ModifyExpressionValue;
+import mod.adrenix.nostalgic.tweak.config.CandyTweak;
+import net.minecraft.server.PlayerAdvancements;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+
+@Mixin(PlayerAdvancements.class)
+public abstract class PlayerAdvancementsMixin
+{
+    /**
+     * Prevents the advancement messages that appear in server chat after a player obtains a new achievement.
+     */
+    @ModifyExpressionValue(
+        method = "award",
+        at = @At(
+            value = "INVOKE",
+            target = "Lnet/minecraft/advancements/DisplayInfo;shouldAnnounceChat()Z"
+        )
+    )
+    private boolean nt_old_hud$shouldAnnounceAdvancementInChat(boolean shouldAnnounceChat)
+    {
+        return !CandyTweak.HIDE_ADVANCEMENT_CHATS.get() && shouldAnnounceChat;
+    }
+}
