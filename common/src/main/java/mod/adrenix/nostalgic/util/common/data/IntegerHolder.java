3ba452d695ad4a0303791c9f571e0fb266dbe0c6
Adrenix <15348635+adrenix@users.noreply.github.com>
2025-01-02 12:47:59 -0500

    Added increment/decrement by amount

--- common/src/main/java/mod/adrenix/nostalgic/util/common/data/IntegerHolder.java
+++ common/src/main/java/mod/adrenix/nostalgic/util/common/data/IntegerHolder.java
@@ -25,15 +25,40 @@ public class IntegerHolder extends Holder<Integer>
     /* Methods */
 
     /**
+     * Get the current value and then increment the held value by the given value.
+     *
+     * @param by The amount to increment by.
+     * @return The held value before it is incremented.
+     */
+    public int getAndIncrement(int by)
+    {
+        int previousValue = this.value;
+
+        this.set(this.value + by);
+
+        return previousValue;
+    }
+
+    /**
      * Get the current value and then increment the held value by one.
      *
      * @return The held value before it is incremented.
      */
     public int getAndIncrement()
     {
+        return getAndIncrement(1);
+    }
+
+    /**
+     * Get the current value and then decrement the held value by the given value.
+     *
+     * @return The held value before it is decremented.
+     */
+    public int getAndDecrement(int by)
+    {
         int previousValue = this.value;
 
-        this.set(this.value + 1);
+        this.set(this.value - by);
 
         return previousValue;
     }
@@ -45,10 +70,6 @@ public class IntegerHolder extends Holder<Integer>
      */
     public int getAndDecrement()
     {
-        int previousValue = this.value;
-
-        this.set(this.value - 1);
-
-        return previousValue;
+        return getAndDecrement(1);
     }
 }
