678e41a5dc214b87c526dc522dc39ab437bb2bfc
Adrenix <15348635+Adrenix@users.noreply.github.com>
2025-01-17 17:18:03 -0500

    Fixed 1.21.4 no custom death loot drops

--- common/src/main/java/mod/adrenix/nostalgic/mixin/tweak/gameplay/mob_drops/MobMixin.java
+++ common/src/main/java/mod/adrenix/nostalgic/mixin/tweak/gameplay/mob_drops/MobMixin.java
@@ -2,6 +2,7 @@ package mod.adrenix.nostalgic.mixin.tweak.gameplay.mob_drops;
 
 import com.llamalad7.mixinextras.injector.v2.WrapWithCondition;
 import mod.adrenix.nostalgic.tweak.config.GameplayTweak;
+import net.minecraft.server.level.ServerLevel;
 import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.Mob;
 import net.minecraft.world.item.ItemStack;
@@ -18,10 +19,10 @@ public abstract class MobMixin
         method = "dropCustomDeathLoot",
         at = @At(
             value = "INVOKE",
-            target = "Lnet/minecraft/world/entity/Mob;spawnAtLocation(Lnet/minecraft/world/item/ItemStack;)Lnet/minecraft/world/entity/item/ItemEntity;"
+            target = "Lnet/minecraft/world/entity/Mob;spawnAtLocation(Lnet/minecraft/server/level/ServerLevel;Lnet/minecraft/world/item/ItemStack;)Lnet/minecraft/world/entity/item/ItemEntity;"
         )
     )
-    private boolean nt_mob_drops$shouldDropCustomDeathLoot(Mob mob, ItemStack itemStack)
+    private boolean nt_mob_drops$shouldDropCustomDeathLoot(Mob mob, ServerLevel level, ItemStack itemStack)
     {
         boolean oldZombiePigman = GameplayTweak.OLD_ZOMBIE_PIGMEN_DROPS.get() && mob.getType() == EntityType.ZOMBIFIED_PIGLIN;
         boolean oldSkeleton = GameplayTweak.OLD_SKELETON_DROPS.get() && mob.getType() == EntityType.SKELETON;
