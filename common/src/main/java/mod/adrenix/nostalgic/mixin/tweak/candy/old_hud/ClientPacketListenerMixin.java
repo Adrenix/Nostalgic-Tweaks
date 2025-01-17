ce5d037678f880e09975c91d772ed83cee92b5da
Adrenix <15348635+Adrenix@users.noreply.github.com>
2025-01-17 17:14:09 -0500

    Fixed 1.21.4 hide recipe toasts

--- common/src/main/java/mod/adrenix/nostalgic/mixin/tweak/candy/old_hud/ClientPacketListenerMixin.java
+++ common/src/main/java/mod/adrenix/nostalgic/mixin/tweak/candy/old_hud/ClientPacketListenerMixin.java
@@ -1,8 +1,10 @@
 package mod.adrenix.nostalgic.mixin.tweak.candy.old_hud;
 
-import com.llamalad7.mixinextras.injector.ModifyExpressionValue;
+import com.llamalad7.mixinextras.injector.v2.WrapWithCondition;
 import mod.adrenix.nostalgic.tweak.config.CandyTweak;
+import net.minecraft.client.gui.components.toasts.ToastManager;
 import net.minecraft.client.multiplayer.ClientPacketListener;
+import net.minecraft.world.item.crafting.display.RecipeDisplay;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 
@@ -12,15 +14,15 @@ public abstract class ClientPacketListenerMixin
     /**
      * Prevents recipe notification toasts from appearing when unlocking a new recipe.
      */
-    @ModifyExpressionValue(
-        method = "method_34011",
+    @WrapWithCondition(
+        method = "handleRecipeBookAdd",
         at = @At(
             value = "INVOKE",
-            target = "Lnet/minecraft/world/item/crafting/Recipe;showNotification()Z"
+            target = "Lnet/minecraft/client/gui/components/toasts/RecipeToast;addOrUpdate(Lnet/minecraft/client/gui/components/toasts/ToastManager;Lnet/minecraft/world/item/crafting/display/RecipeDisplay;)V"
         )
     )
-    private boolean nt_old_hud$shouldShowRecipeToast(boolean showNotification)
+    private boolean nt_old_hud$shouldShowRecipeToast(ToastManager toastManager, RecipeDisplay recipeDisplay)
     {
-        return !CandyTweak.HIDE_RECIPE_TOASTS.get() && showNotification;
+        return !CandyTweak.HIDE_RECIPE_TOASTS.get();
     }
 }
