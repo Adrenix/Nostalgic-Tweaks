3bf083b5299057cb0a9d50ccd867dadcc76993e8
Adrenix <15348635+Adrenix@users.noreply.github.com>
2025-01-17 18:12:01 -0500

    Update native image mixin injector with v2

--- common/src/main/java/mod/adrenix/nostalgic/mixin/required/NativeImageMixin.java
+++ common/src/main/java/mod/adrenix/nostalgic/mixin/required/NativeImageMixin.java
@@ -1,6 +1,6 @@
 package mod.adrenix.nostalgic.mixin.required;
 
-import com.llamalad7.mixinextras.injector.WrapWithCondition;
+import com.llamalad7.mixinextras.injector.v2.WrapWithCondition;
 import com.mojang.blaze3d.platform.NativeImage;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
