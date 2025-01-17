3db3cf8b48fa1cbd5c9b5e9444af41ffbaaafe74
Adrenix <15348635+Adrenix@users.noreply.github.com>
2025-01-17 17:31:50 -0500

    Fixed 1.21.4 Sodium old torches

--- common/src/main/java/mod/adrenix/nostalgic/mixin/sodium/candy/torch_block/BlockRendererMixin.java
+++ common/src/main/java/mod/adrenix/nostalgic/mixin/sodium/candy/torch_block/BlockRendererMixin.java
@@ -2,17 +2,15 @@ package mod.adrenix.nostalgic.mixin.sodium.candy.torch_block;
 
 import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
 import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
-import net.caffeinemc.mods.sodium.client.render.chunk.compile.pipeline.BlockRenderer;
 import mod.adrenix.nostalgic.helper.candy.block.TorchHelper;
 import mod.adrenix.nostalgic.tweak.config.CandyTweak;
+import net.caffeinemc.mods.sodium.client.render.chunk.compile.pipeline.BlockRenderer;
 import net.caffeinemc.mods.sodium.client.render.frapi.mesh.MutableQuadViewImpl;
 import net.fabricmc.fabric.api.renderer.v1.Renderer;
-import net.fabricmc.fabric.api.renderer.v1.RendererAccess;
 import net.fabricmc.fabric.api.renderer.v1.material.RenderMaterial;
 import net.fabricmc.fabric.api.renderer.v1.material.ShadeMode;
 import net.fabricmc.fabric.api.renderer.v1.mesh.QuadEmitter;
 import net.fabricmc.fabric.api.renderer.v1.model.FabricBakedModel;
-import net.fabricmc.fabric.api.renderer.v1.render.RenderContext;
 import net.fabricmc.fabric.api.util.TriState;
 import net.minecraft.client.renderer.block.model.BakedQuad;
 import net.minecraft.client.resources.model.BakedModel;
@@ -27,39 +25,46 @@ import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 
+import java.util.function.Predicate;
 import java.util.function.Supplier;
 
 @Mixin(BlockRenderer.class)
 public abstract class BlockRendererMixin
 {
-    @Unique private static final Renderer RENDERER = RendererAccess.INSTANCE.getRenderer();
-    @Unique private static final RenderMaterial STANDARD_MATERIAL = RENDERER.materialFinder().shadeMode(ShadeMode.VANILLA).find();
-    @Unique private static final RenderMaterial NO_AO_MATERIAL = RENDERER.materialFinder().shadeMode(ShadeMode.VANILLA).ambientOcclusion(TriState.FALSE).find();
+    @Unique private static final RenderMaterial NT$STANDARD_MATERIAL = Renderer.get()
+        .materialFinder()
+        .shadeMode(ShadeMode.VANILLA)
+        .find();
+
+    @Unique private static final RenderMaterial NT$NO_AO_MATERIAL = Renderer.get()
+        .materialFinder()
+        .shadeMode(ShadeMode.VANILLA)
+        .ambientOcclusion(TriState.FALSE)
+        .find();
 
     /**
      * Removes the bottom face quads for a wall torch model.
      */
     @WrapOperation(
-            method = "renderModel",
-            at = @At(
-                    value = "INVOKE",
-                    target = "Lnet/fabricmc/fabric/api/renderer/v1/model/FabricBakedModel;emitBlockQuads(Lnet/minecraft/world/level/BlockAndTintGetter;Lnet/minecraft/world/level/block/state/BlockState;Lnet/minecraft/core/BlockPos;Ljava/util/function/Supplier;Lnet/fabricmc/fabric/api/renderer/v1/render/RenderContext;)V"
-            )
+        method = "renderModel",
+        at = @At(
+            value = "INVOKE",
+            target = "Lnet/fabricmc/fabric/api/renderer/v1/model/FabricBakedModel;emitBlockQuads(Lnet/fabricmc/fabric/api/renderer/v1/mesh/QuadEmitter;Lnet/minecraft/world/level/BlockAndTintGetter;Lnet/minecraft/world/level/block/state/BlockState;Lnet/minecraft/core/BlockPos;Ljava/util/function/Supplier;Ljava/util/function/Predicate;)V"
+        )
     )
-    private void nt_sodium_torch_block$wrapEmitBlockQuads(FabricBakedModel model, BlockAndTintGetter blockAndTintGetter, BlockState blockState, BlockPos blockPos, Supplier<RandomSource> randomSource, RenderContext renderContext, Operation<Void> emitBlockQuads)
+    private void nt_sodium_torch_block$wrapEmitBlockQuads(FabricBakedModel model, QuadEmitter emitter, BlockAndTintGetter blockAndTintGetter, BlockState blockState, BlockPos blockPos, Supplier<RandomSource> randomSupplier, Predicate<Direction> cullTest, Operation<Void> operation)
     {
         if (TorchHelper.isLikeTorch(blockState))
         {
             if (!TorchHelper.isSheared(blockState))
             {
-                emitBlockQuads.call(model, blockAndTintGetter, blockState, blockPos, randomSource, renderContext);
+                operation.call(model, emitter, blockAndTintGetter, blockState, blockPos, randomSupplier, cullTest);
                 return;
             }
 
             final BakedModel vanillaModel = (BakedModel) model;
-            final RandomSource random = randomSource.get();
-            final QuadEmitter emitter = renderContext.getEmitter();
-            final RenderMaterial defaultMaterial = vanillaModel.useAmbientOcclusion() ? STANDARD_MATERIAL : NO_AO_MATERIAL;
+            final RandomSource random = randomSupplier.get();
+            final RenderMaterial defaultMaterial = vanillaModel.useAmbientOcclusion() ? NT$STANDARD_MATERIAL : NT$NO_AO_MATERIAL;
 
             boolean isBottomDisabled = CandyTweak.OLD_TORCH_BOTTOM.get();
 
@@ -69,9 +74,7 @@ public abstract class BlockRendererMixin
             for (BakedQuad quad : TorchHelper.getModel(blockState).getQuads(blockState, null, random))
             {
                 if (isBottomDisabled && quad.getDirection() == Direction.DOWN)
-                {
                     continue;
-                }
 
                 emitter.fromVanilla(quad, defaultMaterial, null);
 
@@ -92,6 +95,6 @@ public abstract class BlockRendererMixin
             }
         }
         else
-            emitBlockQuads.call(model, blockAndTintGetter, blockState, blockPos, randomSource, renderContext);
+            operation.call(model, emitter, blockAndTintGetter, blockState, blockPos, randomSupplier, cullTest);
     }
 }
