210bfc3ad37a197af30c5e859493f231cb59aade
Adrenix <15348635+Adrenix@users.noreply.github.com>
2025-01-18 16:34:58 -0500

    Fixed 1.21.4 uncap title screen FPS

--- common/src/main/java/mod/adrenix/nostalgic/mixin/tweak/candy/uncap_title_fps/MinecraftMixin.java
+++ common/src/main/java/mod/adrenix/nostalgic/mixin/tweak/candy/uncap_title_fps/MinecraftMixin.java
@@ -4,6 +4,10 @@ import com.llamalad7.mixinextras.injector.ModifyReturnValue;
 import com.mojang.blaze3d.platform.Window;
 import mod.adrenix.nostalgic.tweak.config.CandyTweak;
 import net.minecraft.client.Minecraft;
+import net.minecraft.client.Options;
+import net.minecraft.client.multiplayer.ClientLevel;
+import org.jetbrains.annotations.Nullable;
+import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
@@ -11,18 +15,31 @@ import org.spongepowered.asm.mixin.injection.At;
 @Mixin(Minecraft.class)
 public abstract class MinecraftMixin
 {
+    /* Shadows */
+
     @Shadow
     public abstract Window getWindow();
+
+    @Shadow @Final public Options options;
+    @Shadow @Nullable public ClientLevel level;
+
+    /* Injections */
 
     /**
      * Uncaps the framerate limit imposed on the title screen.
      */
     @ModifyReturnValue(
-        method = "getFramerateLimit",
-        at = @At("RETURN")
+        method = "runTick",
+        at = @At(
+            value = "INVOKE",
+            target = "Lcom/mojang/blaze3d/platform/FramerateLimitTracker;getFramerateLimit()I"
+        )
     )
     private int nt_uncap_title_fps$modifyLimit(int framerate)
     {
-        return CandyTweak.UNCAP_TITLE_FPS.get() ? Math.max(this.getWindow().getFramerateLimit(), 60) : framerate;
+        if (this.level != null)
+            return framerate;
+
+        return CandyTweak.UNCAP_TITLE_FPS.get() ? Math.max(this.options.framerateLimit().get(), 60) : framerate;
     }
 }
