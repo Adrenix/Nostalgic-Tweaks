0af1557e90914a766481e40b8c2b28d5e545a349
Adrenix <15348635+adrenix@users.noreply.github.com>
2026-08-11 19:24:11 -0400

    fix(config): incorrect json key for server full hitbox targeting

--- common/src/main/java/mod/adrenix/nostalgic/config/ServerConfig.java
+++ common/src/main/java/mod/adrenix/nostalgic/config/ServerConfig.java
@@ -53,7 +53,7 @@ public class ServerConfig implements ConfigMeta
     public static class EyeCandy
     {
         public boolean hideAdvancementChats = CandyTweak.HIDE_ADVANCEMENT_CHATS.register("hideAdvancementChats");
-        public boolean applyFullHitboxTargeting = CandyTweak.APPLY_FULL_HITBOX_TARGETING.register("applyFullBlockTargeting");
+        public boolean applyFullHitboxTargeting = CandyTweak.APPLY_FULL_HITBOX_TARGETING.register("applyFullHitboxTargeting");
         public boolean applyFullBlockCollisions = CandyTweak.APPLY_FULL_BLOCK_COLLISIONS.register("applyFullBlockCollisions");
         public Hotbar oldCreativeHotbar = CandyTweak.OLD_CREATIVE_HOTBAR.register("oldCreativeHotbar");
         public int itemMergeLimit = CandyTweak.ITEM_MERGE_LIMIT.register("itemMergeLimit");
