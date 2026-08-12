18e15b36f1741a849a9309c33ba8202844363ced
Adrenix <15348635+adrenix@users.noreply.github.com>
2026-08-11 22:17:35 -0400

    fix(tweak): prevent AppleSkin hud elements rendering when hunger is disabled

--- fabric/src/main/java/mod/adrenix/nostalgic/fabric/event/AppleSkinListener.java
+++ fabric/src/main/java/mod/adrenix/nostalgic/fabric/event/AppleSkinListener.java
@@ -3,6 +3,7 @@ package mod.adrenix.nostalgic.fabric.event;
 import mod.adrenix.nostalgic.NostalgicTweaks;
 import mod.adrenix.nostalgic.tweak.config.GameplayTweak;
 import squeek.appleskin.api.AppleSkinApi;
+import squeek.appleskin.api.event.HUDOverlayEvent;
 import squeek.appleskin.api.event.TooltipOverlayEvent;
 
 public class AppleSkinListener implements AppleSkinApi
@@ -16,6 +17,11 @@ public class AppleSkinListener implements AppleSkinApi
         TooltipOverlayEvent.Pre.EVENT.register(AppleSkinListener::disableTooltip);
         TooltipOverlayEvent.Render.EVENT.register(AppleSkinListener::disableTooltip);
 
+        HUDOverlayEvent.Exhaustion.EVENT.register(AppleSkinListener::disableHUD);
+        HUDOverlayEvent.Saturation.EVENT.register(AppleSkinListener::disableHUD);
+        HUDOverlayEvent.HungerRestored.EVENT.register(AppleSkinListener::disableHUD);
+        HUDOverlayEvent.HealthRestored.EVENT.register(AppleSkinListener::disableHUD);
+
         NostalgicTweaks.LOGGER.info("Registered Fabric AppleSkin Listener");
     }
 
@@ -25,6 +31,17 @@ public class AppleSkinListener implements AppleSkinApi
      * @param event The {@link TooltipOverlayEvent} instance.
      */
     private static void disableTooltip(TooltipOverlayEvent event)
+    {
+        if (GameplayTweak.DISABLE_HUNGER.get())
+            event.isCanceled = true;
+    }
+
+    /**
+     * Prevents HUD elements from being drawn when hunger is disabled.
+     *
+     * @param event The {@link HUDOverlayEvent} instance.
+     */
+    private static void disableHUD(HUDOverlayEvent event)
     {
         if (GameplayTweak.DISABLE_HUNGER.get())
             event.isCanceled = true;
--- neoforge/src/main/java/mod/adrenix/nostalgic/neoforge/event/AppleSkinHandler.java
+++ neoforge/src/main/java/mod/adrenix/nostalgic/neoforge/event/AppleSkinHandler.java
@@ -2,30 +2,45 @@ package mod.adrenix.nostalgic.neoforge.event;
 
 import mod.adrenix.nostalgic.tweak.config.GameplayTweak;
 import net.neoforged.bus.api.SubscribeEvent;
+import squeek.appleskin.api.event.HUDOverlayEvent;
 import squeek.appleskin.api.event.TooltipOverlayEvent;
 
 public abstract class AppleSkinHandler
 {
-    /**
-     * Prevents tooltip food data from rendering if hunger is disabled.
-     *
-     * @param event The {@link TooltipOverlayEvent.Render} instance.
-     */
     @SubscribeEvent
     public static void onRenderTooltipEvent(TooltipOverlayEvent.Render event)
     {
         disableTooltip(event);
     }
 
-    /**
-     * Prevents tooltip food data pre-processing if hunger is disabled.
-     *
-     * @param event The {@link TooltipOverlayEvent.Pre} instance.
-     */
     @SubscribeEvent
     public static void onPreTooltipEvent(TooltipOverlayEvent.Pre event)
     {
         disableTooltip(event);
+    }
+
+    @SubscribeEvent
+    public static void onRenderHUDExhaustion(HUDOverlayEvent.Exhaustion event)
+    {
+        disableHUD(event);
+    }
+
+    @SubscribeEvent
+    public static void onRenderHUDSaturation(HUDOverlayEvent.Saturation event)
+    {
+        disableHUD(event);
+    }
+
+    @SubscribeEvent
+    public static void onRenderHUDHungerRestored(HUDOverlayEvent.HungerRestored event)
+    {
+        disableHUD(event);
+    }
+
+    @SubscribeEvent
+    public static void onRenderHUDHealthRestored(HUDOverlayEvent.HealthRestored event)
+    {
+        disableHUD(event);
     }
 
     /**
@@ -34,6 +49,17 @@ public abstract class AppleSkinHandler
      * @param event The {@link TooltipOverlayEvent} instance.
      */
     private static void disableTooltip(TooltipOverlayEvent event)
+    {
+        if (GameplayTweak.DISABLE_HUNGER.get())
+            event.setCanceled(true);
+    }
+
+    /**
+     * Prevents HUD elements from being drawn when hunger is disabled.
+     *
+     * @param event The {@link HUDOverlayEvent} instance.
+     */
+    private static void disableHUD(HUDOverlayEvent event)
     {
         if (GameplayTweak.DISABLE_HUNGER.get())
             event.setCanceled(true);
